75be0cbe7921fdd25c88f2c8ca4b5db375e2468f
dark1zinn <edilsonjuininho154@gmail.com>
2025-04-28 16:36:13 -0300

    🔧 Wrap navigate in splash screen in useEffect

--- src/app/splash-screen.tsx
+++ src/app/splash-screen.tsx
@@ -1,13 +1,17 @@
 // import { invoke } from "@tauri-apps/api/core";
 import { PackageOpen } from "lucide-react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 export default function SplashScreen() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
+  useEffect(() => {
+    console.log("Performing setup tasks...")
     setTimeout(() => {  //* Must not leave this here, its just so i can view the splash screen
-        navigate("/login");
+      navigate("/login");
     }, 3300);
+  });
 
   return (  // TODO: make bg image on spalsh screen work
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10" style={{ backgroundImage: `url("../assets/splash-screen.png")`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
