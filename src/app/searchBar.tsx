35546e43f0c9fcaee712fa5c2daf6d7adf81daa8
dark1zinn <edilsonjuininho154@gmail.com>
2025-05-01 01:27:59 -0300

    🔧 Small fix

--- src/app/searchBar.tsx
+++ src/app/searchBar.tsx
@@ -13,12 +13,12 @@ export default function SearchBar() {
                 value={searchValue}
                 placeholder="Ctrl + K"
                 onChange={(e) => setSearchValue(e.target.value)}
-                className="peer w-full ease-in-out transition-all duration-400 h-full placeholder:pl-5 placeholder:font-semibold placeholder:ease-in-out placeholder:duration-400 focus:placeholder:opacity-0" />
-            <Search className="size-4 stroke-3 absolute left-2 ease-in-out transition-all duration-300 peer-focus:opacity-0 peer-[:not(:placeholder-shown)]:opacity-0" />
+                className="peer w-full ease-in-out transition-all duration-400 h-full placeholder:text-center placeholder:font-semibold placeholder:ease-in-out placeholder:duration-400 focus:placeholder:opacity-0" />
+            <Search className="size-4 stroke-3 opacity-50 absolute left-2 ease-in-out transition-all duration-300 peer-focus:opacity-0 peer-[:not(:placeholder-shown)]:opacity-0" />
             <X
                 onClick={() => setSearchValue('')}
                 onMouseDown={(e) => e.preventDefault()}
-                className="size-4 stroke-3 absolute right-0 ease-in-out transition-all duration-300 opacity-0 peer-[input:not(:placeholder-shown)]:opacity-100 peer-focus:right-3 hover:shadow-accent hover:stroke-red-400" />
+                className="size-4 stroke-3 absolute right-0 ease-in-out transition-all duration-300 opacity-0 peer-[input:not(:placeholder-shown)]:opacity-100 peer-[input:not(:placeholder-shown)]:right-3 peer-focus:right-3 hover:shadow-accent hover:stroke-red-400" />
         </div>
     )
 }
