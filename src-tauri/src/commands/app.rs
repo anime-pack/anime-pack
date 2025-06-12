4d1c8b53168abe7710081aa047cefbfe12f56ad9
dark1zinn <edilsonjuininho154@gmail.com>
2025-06-12 00:21:17 -0300

    🚿 Remove unused tauri command

--- src-tauri/src/commands/app.rs
+++ src-tauri/src/commands/app.rs
@@ -1,22 +1,6 @@
-use tauri::Manager;
 use tauri_plugin_opener::OpenerExt;
 
 //* App utilityes related:
-
-//? Controls the webview window behavior to close, minimize, maximize
-#[tauri::command]
-pub fn window_mmc(app: tauri::AppHandle, window_label: &str, action: &str) {
-    let window = app.get_webview_window(window_label).unwrap();
-    if action == "minimize" {
-        window.minimize().unwrap();
-    } else if action == "maximize" {
-        window.maximize().unwrap();
-    } else if action == "unmaximize" {
-        window.unmaximize().unwrap();
-    } else if action == "close" {
-        window.close().unwrap();
-    }
-}
 
 //? Opens urls in the user default browser
 #[tauri::command]
